d7dd35f72b1322a8b90135180446aee41b1444d5
Cidorvan Leite <cidorvan@gmail.com>
2023-10-17 19:15:47 +0200

    GA: added nationalization support

--- ga.c
+++ ga.c
@@ -3,14 +3,21 @@
 
 #define DBG_TITLE "\033[1;35mGA:\033[0m "
 
-uint8_t joyP1 = 0xFF, joyP2 = 0xFF;
+uint8_t nationalization = 0x00, joyP1 = 0xFF, joyP2 = 0xFF;
 
 uint8_t readGAJoyP1() {
     return joyP1;
 }
 
 uint8_t readGAJoyP2() {
-    return joyP2;
+    uint8_t mask = 0xFF;
+
+    if (!(nationalization & 0x80))
+        mask &= ~0x80;
+    if (!(nationalization & 0x20))
+        mask &= ~0x40;
+
+    return joyP2 & mask;
 }
 
 void writeGAMemoryControl(uint8_t value) {
@@ -18,5 +25,6 @@ void writeGAMemoryControl(uint8_t value) {
 }
 
 void writeGAJoyControl(uint8_t value) {
+    nationalization = value;
     DBG_PRINT("writing %02X to joy control\n", value);
 }
